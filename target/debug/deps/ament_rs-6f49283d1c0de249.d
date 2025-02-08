2062ab916f2a9e6ca4b1a6d97fee4c4215819212
Gérald Lelong <gerald.lelong@easymov.fr>
2025-02-08 11:37:09 +0100

    add devenv

---
+++ target/debug/deps/ament_rs-6f49283d1c0de249.d
@@ -0,0 +1,9 @@
+/mnt/wd/sisyphe/Projects/opensource/ament_rs/target/debug/deps/ament_rs-6f49283d1c0de249: src/lib.rs src/ament.rs src/packages.rs src/resources.rs src/search_paths.rs
+
+/mnt/wd/sisyphe/Projects/opensource/ament_rs/target/debug/deps/ament_rs-6f49283d1c0de249.d: src/lib.rs src/ament.rs src/packages.rs src/resources.rs src/search_paths.rs
+
+src/lib.rs:
+src/ament.rs:
+src/packages.rs:
+src/resources.rs:
+src/search_paths.rs:
---
+++ target/debug/examples/package_list-3dc7b8d5ec276779.d
@@ -0,0 +1,5 @@
+/mnt/wd/sisyphe/Projects/opensource/ament_rs/target/debug/examples/package_list-3dc7b8d5ec276779: examples/package_list.rs
+
+/mnt/wd/sisyphe/Projects/opensource/ament_rs/target/debug/examples/package_list-3dc7b8d5ec276779.d: examples/package_list.rs
+
+examples/package_list.rs:
